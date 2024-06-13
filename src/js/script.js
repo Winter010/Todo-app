712b3075b675fba7f82d2c76b2ac5f8b50abf75a
Winter010 <146598719+Winter010@users.noreply.github.com>
2024-06-14 04:17:21 +1000

    added tasksArray, add and delete element in array

--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,54 @@
 const input = document.getElementById("task-input");
-
 const tasksList = document.querySelector(".todo-list");
-
 const itemsLeft = document.querySelector(".items-left");
 
 const displayItemsLeft = () => {
 	const allTasks = document.querySelectorAll(".todo-list__item");
-	itemsLeft.innerHTML = `${allTasks.length} items left`;
+	itemsLeft.innerText = `${allTasks.length} items left`;
 };
 
 displayItemsLeft();
 
+const tasksArray = [];
+
 const addTask = event => {
-	const taskTemplate = `
-<li class="todo-list__item">
-	<label>
-		<input type="checkbox" name="complete-task" />
-		<div class="todo-list__checkmark"></div>
-		<span>${input.value}</span>
-	</label>
-	<div class="todo-list__remove-item" data-action="delete"></div>
-</li>
-	`;
+	const taskText = input.value;
 
-	if (event.key === "Enter") {
-		if (input.value.trim() !== "") {
-			tasksList.insertAdjacentHTML("beforeend", taskTemplate);
-			input.value = "";
-		}
+	if (event.key === "Enter" && taskText.trim() !== "") {
+		const newTask = {
+			id: Date.now(),
+			text: taskText,
+			isDone: false,
+		};
+		const taskTemplate = `
+			<li class="todo-list__item" id="${newTask.id}">
+				<label>
+					<input type="checkbox" name="complete-task" />
+					<div class="todo-list__checkmark"></div>
+					<span>${newTask.text}</span>
+				</label>
+				<div class="todo-list__remove-item" data-action="delete"></div>
+			</li>
+		`;
+
+		tasksArray.push(newTask);
+		tasksList.insertAdjacentHTML("beforeend", taskTemplate);
+		input.value = "";
+
+		displayItemsLeft();
 	}
-
-	displayItemsLeft();
 };
 
 const deleteTask = event => {
 	if (event.target.dataset.action === "delete") {
 		const parentNode = event.target.closest("li");
+		const index = tasksArray.findIndex(task => task.id == parentNode.id);
+		tasksArray.splice(index, 1);
+
 		parentNode.remove();
+		displayItemsLeft();
 	}
-	displayItemsLeft();
 };
 
 input.addEventListener("keypress", addTask);
-
 tasksList.addEventListener("click", deleteTask);
